c66e84a7de912df4b98af09cd3af10b449dff263
mohamed <mo7hamedma7hmod@gmail.com>
2021-03-23 11:49:09 +0200

    the Login end point Done

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const ourRoutes = require("./routes");
+const ourRoutes = require("./routes/Auth");
 bodyParser = require("body-parser");
 const _PORT = 5555;
 //app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +16,7 @@ mongoose
   .then(() => console.log("connecting to database successful"))
   .catch((err) => console.error("could not connect to mongo DB", err));
 
-app.use("/api", ourRoutes);
+app.use("/api/Auth", ourRoutes);
 
 app.listen(_PORT, () => {
   console.log("Server Is Running On 5555");
--- BackEnd/routes/Auth.js
+++ BackEnd/routes/Auth.js
@@ -1,9 +1,9 @@
 //for encrypt the password before saving into the database
 //const bcrypt = require("bcrypt");
 const express = require("express");
-const { User, schemaValidate, findOne } = require("./models/user");
+const { User, schemaValidate, findOne } = require("../models/user");
 const router = express.Router();
-
+// register End Point
 router.post("/register", async (req, res) => {
   // First Validate The Request
   const { error } = schemaValidate.validate(req.body);
@@ -30,6 +30,22 @@ router.post("/register", async (req, res) => {
 
     res.send(user);
   }
+});
+//Login End Point
+router.post("/login", async (req, res) => {
+  let user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+    type: req.body.type,
+  });
+  if (!user) {
+    return res.status(400).json({
+      message: "User Not Exist",
+    });
+  } else
+    res.status(200).json({
+      message: "User Logged in Successfully",
+    });
 });
 
 module.exports = router;
