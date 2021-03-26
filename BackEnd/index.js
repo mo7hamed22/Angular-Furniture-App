7cc38321e136299c61d90e77d27111d2a90e11eb
mohamed <mo7hamedma7hmod@gmail.com>
2021-03-26 17:52:34 +0200

    Connect To Atlas DB

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,12 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-var uri = "mongodb://localhost:27017/furnitureApp";
+//var uri = "mongodb://localhost:27017/furnitureApp";
+var _uri =
+  "mongodb+srv://Mohamed:NBTz266H5uZ4gg6K@e-commerce.bqrge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(_uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("connecting to database successful"))
   .catch((err) => console.error("could not connect to mongo DB", err));
+
 app.use("/api/product", productRoute);
 
 app.use("/api/Auth", authRoute);
